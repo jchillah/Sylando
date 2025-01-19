6940b73d5aae2f69837dd6973547cbe029697137
Jchillah <jchillah@gmail.com>
2025-01-19 16:49:37 +0100

    Initial Commit

---
+++ Sylando/SylandoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SylandoApp.swift
+//  Sylando
+//
+//  Created by Michael Winkler on 19.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SylandoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
